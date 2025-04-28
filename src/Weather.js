e709b75bd835ff3044f07514efa8cdbf10ccda6c
jinghui <934192776@qq.com>
2025-04-28 23:19:20 +1000

    update

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
+import { ClipLoader } from "react-spinners";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
