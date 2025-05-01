282a096c37642fb8b2995ce9e31245a8e2e8ccfb
jinghui <934192776@qq.com>
2025-05-01 21:34:36 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import Weather from "./Weather";
 import WeatherInfo from "./WeatherInfo";
 import "./App.css";
